da67abcb1631da743d49c21c45e1b5bcd945a6fd
Hetarth Chopra <34271010+choprahetarth@users.noreply.github.com>
2023-07-13 14:15:41 +0000

    scrape resume error in generate linkedin_dm

--- api/index.py
+++ api/index.py
@@ -165,20 +165,21 @@ def research_info(email):
 #             "resume": scrape_resume(file_in_memory),
 #             "temperature_setting" : form.temperature_setting.data
 #         }
-#
+
 #         linkedin_dm = generate_industry_linkedin_dm(data)
 #         data["linkedin_dm"] = linkedin_dm
-#
+
 #         mongo.db.users.update_one(
 #             {"email": email},
 #             {"$push": {"submissions": data},
 #              "$inc": {"credits": -10}},  # Reduce credits by 10
 #             upsert=True
 #         )
-#
+
 #         return jsonify(data["linkedin_dm"])
-#
-#     return render_template('job.html', form=form)
+
+#     # return render_template('job.html', form=form)
+#     return linkedin_dm
 
 
 def allowed_file(filename):
@@ -315,10 +316,22 @@ def generate_research_mail(data):
 
 @app.route("/generate_industry_linkedin_dm", methods=["POST"])
 def generate_industry_linkedin_dm():
+    # form = JobForm()
+    # if form.validate_on_submit():
+    #     file_in_memory = form.resume.data
+    #     data = {
+    #         "email": email,
+    #         "person_name": form.person_name.data,
+    #         "gpt_option": form.gpt_option.data,
+    #         "option": "job",
+    #         "job_description": form.job_description.data,
+    #         "resume": scrape_resume(file_in_memory),
+    #         "temperature_setting" : form.temperature_setting.data
+    #     }
     email = request.form.get("email")
     job_description = request.form.get("job_description")
     job_description = job_description[:1000] # HARD LIMIT 1 
-    resume = request.form.get("resume")
+    resume = scrape_resume(request.form.get("resume"))
     resume = resume[:1000] # HARD LIMIT 2
     name_of_referrer = request.form.get("person_name")
     gpt_name = request.form.get("gpt_option")
